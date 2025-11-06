0b0e93fdde9cb48332680bf6292d85460abf63dd
wangzikang-wuhan <imwuhanwangzikang@gmail.com>
2025-11-06 17:54:57 +0800

    [new]基础语义检索已实现

---
+++ ai_code_search_engine/starter.py
@@ -0,0 +1,18 @@
+from code_cutting.springboot_project_parse import SpringBootParser
+
+if __name__ == '__main__':
+    print("开始加载项目")
+    results = SpringBootParser()
+    
+
+
+
+
+
+
+
+
+
+
+
+
